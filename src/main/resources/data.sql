101c2a41ec3a2bc1232a037329bfae3e677ac7b1
Rachel Titco <rachel.titco@gmail.com>
2020-12-05 18:40:46 -0500

    dob

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,4 +2,4 @@ INSERT INTO client (id, address, city, country, postal_code, email, first_name, 
 (1, '123 Test Street', 'Toronto', 'Canada', 'A1A 1A1', 'admin@isp.net', 'John', 'Doe', '1969-01-01', 'P@ssword1', 'Admin');
 
 INSERT INTO client (id, address, city, country, postal_code, email, first_name, last_name, dob, password, role) VALUES
-(2, '123 Test Street', 'Toronto', 'Canada', 'A1A 1A1', 'client@isp.net', 'Jane', 'Smith', 'P@ssword1', '1990-01-01', 'Client');
+(2, '123 Test Street', 'Toronto', 'Canada', 'A1A 1A1', 'client@isp.net', 'Jane', 'Smith','1990-01-01', 'P@ssword1',  'Client');
