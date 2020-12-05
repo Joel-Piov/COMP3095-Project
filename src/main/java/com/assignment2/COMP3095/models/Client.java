ccb0e7babc001e1396136c679d5bfd43683ef078
Rachel Titco <rachel.titco@gmail.com>
2020-12-05 17:07:39 -0500

    client dashboard shows client profile

--- src/main/java/com/assignment2/COMP3095/models/Client.java
+++ src/main/java/com/assignment2/COMP3095/models/Client.java
@@ -13,40 +13,40 @@ import javax.persistence.*;
 import javax.validation.constraints.*;
 
 @Entity
-@SequenceGenerator(name="client_id_seq", initialValue=3, allocationSize=100)
+@SequenceGenerator(name = "client_id_seq", initialValue = 3, allocationSize = 100)
 public class Client {
 
 
     @Id
-    @GeneratedValue(strategy=GenerationType.SEQUENCE, generator="client_id_seq")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "client_id_seq")
     //@OneToMany(mappedBy="card", fetch = FetchType.LAZY, cascade = CascadeType.PERSIST)
     private long id;
 
-    @NotEmpty(message="Email field should not be empty")
-    @Email(regexp="^(.+)@(.+)$", message="Invalid email pattern")
+    @NotEmpty(message = "Email field should not be empty")
+    @Email(regexp = "^(.+)@(.+)$", message = "Invalid email pattern")
     private String email;
 
     @NotNull
-    @Size(min=6,max=12,message="Password should be between 6 and 12 characters.")
-    @Pattern(regexp="^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{6,12}$", message="Password must contain at least 1 uppercase, 1 special character and 1 number")
+    @Size(min = 6, max = 12, message = "Password should be between 6 and 12 characters.")
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{6,12}$", message = "Password must contain at least 1 uppercase, 1 special character and 1 number")
     private String password;
 
     @NotNull
-    @Size(min=2,max=32,message="First Name should be more than 2 characters and less than 32.")
-    @Pattern(regexp = "^[A-z]+$", message="First Name must contain only letters (a-Z)")
+    @Size(min = 2, max = 32, message = "First Name should be more than 2 characters and less than 32.")
+    @Pattern(regexp = "^[A-z]+$", message = "First Name must contain only letters (a-Z)")
     private String firstName;
 
     @NotNull
-    @Size(min=2,max=32,message="Last Name should be more than 2 characters and less than 32.")
-    @Pattern(regexp = "^[A-z]+$", message="Last Name must contain only letters (a-Z)")
+    @Size(min = 2, max = 32, message = "Last Name should be more than 2 characters and less than 32.")
+    @Pattern(regexp = "^[A-z]+$", message = "Last Name must contain only letters (a-Z)")
     private String lastName;
 
     @NotNull
-    @Pattern(regexp = "^\\d+\\s[A-z]+\\s[A-z]+$", message="Invalid Address - (ex: 123 Example Street)")
+    @Pattern(regexp = "^\\d+\\s[A-z]+\\s[A-z]+$", message = "Invalid Address - (ex: 123 Example Street)")
     private String address;
 
     @NotNull
-    @Size(min=2,message="City should be more than 2 characters.")
+    @Size(min = 2, message = "City should be more than 2 characters.")
     private String city;
 
     @NotNull
@@ -58,7 +58,10 @@ public class Client {
 
     private String role;
 
-    public long getId() { return id; }
+    public long getId() {
+        return id;
+    }
+
     public String getPassword() {
         return password;
     }
@@ -67,7 +70,9 @@ public class Client {
         this.password = password;
     }
 
-    public void setRole(String role) { this.role = role; }
+    public void setRole(String role) {
+        this.role = role;
+    }
 
     public String getRole() {
         return role;
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
 INSERT INTO client (id, address, city, country, postal_code, email, first_name, last_name, password, role) VALUES
-(1, '123 Test Street', '123 Admin St', 'Toronto', 'Canada', 'A1A 1A1', 'admin@isp.net', 'John', 'Doe', 'P@ssword1', 'Admin');
+(1, '123 Test Street', 'Toronto', 'Canada', 'A1A 1A1', 'admin@isp.net', 'John', 'Doe', 'P@ssword1', 'Admin');
 
-INSERT INTO client (id, address, city, country, email, first_name, last_name, password, role) VALUES
-(2, '123 Test Street', '123 Admin St', 'Toronto', 'Canada', 'A1A 1A1', 'client@isp.net', 'Jane', 'Smith', 'P@ssword1', 'Client');
+INSERT INTO client (id, address, city, country, postal_code, email, first_name, last_name, password, role) VALUES
+(2, '123 Test Street', 'Toronto', 'Canada', 'A1A 1A1', 'client@isp.net', 'Jane', 'Smith', 'P@ssword1', 'Client');
